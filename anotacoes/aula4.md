74dd13cc3ec06501f2b5cb541da70f8e16a8b1ce
Beatriz Almeida <beatrizalmeidabss@gmail.com>
2025-09-09 20:13:01 -0300

    feat: tipando informações no index

---
+++ anotacoes/aula4.md
@@ -0,0 +1,35 @@
+## Código do index sem a tipagem (ainda em JS):
+
+
+```js
+import express from "express";
+
+const app = express();
+
+const user = [
+    {nome: 'carlos', idade: '23'}, 
+    {nome: 'gabriela', idade: '23'}
+]
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Bem vindo")
+});
+
+app.get("/users", (req, res) => {
+    res.json(user);
+});
+
+app.post("/users", (req, res) => {
+    const novoUser = req.body;
+    user.push(novoUser);
+    res.json(user);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(API rodando na porta)
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,37 @@
 import express from "express";
+import { Express, Request, Response }  from "express"; // importanto tipos do Express
 
-const app = express();
 
-const user = [
-    {nome: 'carlos', idade: '23'}, 
-    {nome: 'gabriela', idade: '23'}
-]
+
+const app: Express = express(); // o aplicativo é do framework Express.js, então tem que receber o tipo do framework
+const port: number = 3000; // a porta na qual ele vai rodar é int
 
 app.use(express.json());
 
+
+// criando um novo tipo
+type User = {
+    id: number;
+    nome: string;
+    idade: number;
+}
+
+
+const user: User[] = [ // tipando o array de usuários com o tipo User criado
+    {
+        id: 1,
+        nome: 'Beatriz',
+        idade: 23
+    },
+    {
+        id: 2,
+        nome: 'Guilherme',
+        idade: 21
+    }
+]
+
+
+// ENDPOINTS ----
 app.get("/", (req, res) => {
     res.send("Bem vindo")
 });
@@ -22,8 +45,6 @@ app.post("/users", (req, res) => {
     user.push(novoUser);
     res.json(user);
 });
-
-const port = 3000;
 
 app.listen(port, () => {
     console.log(API rodando na porta)
