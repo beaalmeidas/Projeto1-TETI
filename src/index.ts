52e05fed860080482ef7aeb859819af6fec51c35
Beatriz Almeida <beatrizalmeidabss@gmail.com>
2025-09-10 10:42:52 -0300

    feat: começando a adaptação de alguns endpoints para a tipagem

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { Express, Request, Response }  from "express"; // importanto tipos do Express
+import { Express, Request, Response }  from "express";      // importanto tipos do Express
 
 
 
-const app: Express = express(); // o aplicativo é do framework Express.js, então tem que receber o tipo do framework
-const port: number = 3000; // a porta na qual ele vai rodar é int
+const app: Express = express();         // o aplicativo é do framework Express.js, então tem que receber o tipo do framework
+const port: number = 3000;              // a porta na qual ele vai rodar é int
 
 app.use(express.json());
 
@@ -17,7 +17,7 @@ type User = {
 }
 
 
-const user: User[] = [ // tipando o array de usuários com o tipo User criado
+const users: User[] = [          // tipando o array de usuários com o tipo User criado
     {
         id: 1,
         nome: 'Beatriz',
@@ -36,16 +36,67 @@ app.get("/", (req, res) => {
     res.send("Bem vindo")
 });
 
-app.get("/users", (req, res) => {
-    res.json(user);
+// get de todos os usuários
+app.get("/users", (req: Request, res: Response) => {
+    res.json(users);
 });
 
-app.post("/users", (req, res) => {
+// get de usuário específico
+app.get("/users/:id", (req: Request, res: Response) => {
+
+    // parsando de string para int, por que o id vem na url como string
+    // o 10 serve para o parser saber que o número está na escala decimal
+    const id: number = parseInt(req.params.id, 10);
+
+
+});
+
+// 
+app.post("/users", (req: Request, res: Response) => {
+    // vai receber o novo usuário criado
     const novoUser = req.body;
-    user.push(novoUser);
-    res.json(user);
+
+    /* VERSÃO SEM REDUCE:
+    // variável iniciada com 0 por que inicialmente não tem nenhum id cadastrado
+    let maxId: number = 0;
+
+    // atualiza qual o maior id (será o último usuário cadastrado)
+    for (const user of users) {
+        if (novoUser.id > maxId) {
+            maxId: novoUser.id;
+        }
+    }
+    */
+
+    // VERSÃO COM REDUCE:
+    const maxId: number = users.reduce((max, user) => (user.id > max ? user.id : max), 0);
+
+    users.push(novoUser);
+    res.status(201).json("Usuário cadastrado com sucesso!");
+});
+
+// atualização de usuário
+app.put("/users/:id", (req: Request, res: Response) => {
+    const userId: number = parseInt(req.params.id, 10);
+    
+    const 
+});
+
+// delete de usuário
+app.delete("/users/:id", (req: Request, res: Response) => {
+    const userId: number = parseInt(req.params.id, 10);
+    const userIndex: number = users.findIndex(user => user.id == userId);
+
+    if (userIndex == -1) {
+        res.status(404).json("Usuário não encontrado!");
+    }
+
+    // deletando:
+    users.splice(userIndex);
+
+    res.status
 });
 
 app.listen(port, () => {
-    console.log(API rodando na porta)
+    console.log("A API subiu na porta ${port}")
 });
