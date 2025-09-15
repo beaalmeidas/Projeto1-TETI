aa383f76b3a0550a3d07dfea5aae5e813f972aab
Beatriz Almeida <beatrizalmeidabss@gmail.com>
2025-09-15 19:14:45 -0300

    feat: finalizando código e adicionando anotações no endpoint de get by id

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { Express, Request, Response }  from "express";      // importanto tipos do Express
+import { Express, Request, Response }  from "express";      // importando tipos do Express
 
 
 
@@ -33,13 +33,16 @@ const users: User[] = [          // tipando o array de usuários com o tipo User
 
 // ENDPOINTS ----
 app.get("/", (req, res) => {
-    res.send("Bem vindo")
+    res.send("Página inicial")
 });
 
+
 // get de todos os usuários
-app.get("/users", (req: Request, res: Response) => {
+// aqui só se passa o res, por que não se pega nada do usuário por req
+app.get("/users", (res: Response) => {
     res.json(users);
 });
+
 
 // get de usuário específico
 app.get("/users/:id", (req: Request, res: Response) => {
@@ -48,10 +51,24 @@ app.get("/users/:id", (req: Request, res: Response) => {
     // o 10 serve para o parser saber que o número está na escala decimal
     const id: number = parseInt(req.params.id, 10);
 
+    if (isNaN(id) || id <= 0) {
+        return res.status(400).json({message: "Id inválido."});
+    }
 
+    // procura o usuário cujo id é igual ao id que veio na url
+    // caso não encontrar, user fica como underfined
+    // caso encontrar, foundUser fica como o objeto retornado em user
+    const foundUser = users.find(user => user.id === id);
+
+    if (!foundUser) {
+        return res.status(404).json({ message: "Usuário não encontrado." });
+    }
+
+    res.json(foundUser);
 });
 
-// 
+
+// cadastro de usuário
 app.post("/users", (req: Request, res: Response) => {
     // vai receber o novo usuário criado
     const novoUser = req.body;
@@ -75,12 +92,14 @@ app.post("/users", (req: Request, res: Response) => {
     res.status(201).json("Usuário cadastrado com sucesso!");
 });
 
+
 // atualização de usuário
 app.put("/users/:id", (req: Request, res: Response) => {
     const userId: number = parseInt(req.params.id, 10);
     
     const 
 });
+
 
 // delete de usuário
 app.delete("/users/:id", (req: Request, res: Response) => {
@@ -96,6 +115,7 @@ app.delete("/users/:id", (req: Request, res: Response) => {
 
     res.status
 });
+
 
 app.listen(port, () => {
     console.log("A API subiu na porta ${port}")
