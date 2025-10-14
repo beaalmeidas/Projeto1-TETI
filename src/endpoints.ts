e5fc272e1b213aac3c55deb69c16bcac89ffaa8d
Beatriz Almeida <beatrizalmeidabss@gmail.com>
2025-10-14 19:07:12 -0300

    feat: mudando arquivo principal para o padrão correto (+ implementando ServiceError para tratamentos futuros)

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,36 +1,3 @@
-import express from "express";
-import { Express, Request, Response }  from "express";      // importando tipos do Express
-
-
-
-const app: Express = express();         // o aplicativo é do framework Express.js, então tem que receber o tipo do framework
-const port: number = 3000;              // a porta na qual ele vai rodar é int
-
-app.use(express.json());
-
-
-// criando um novo tipo
-type User = {
-    id: number;
-    nome: string;
-    idade: number;
-}
-
-
-const users: User[] = [          // tipando o array de usuários com o tipo User criado
-    {
-        id: 1,
-        nome: 'Beatriz',
-        idade: 23
-    },
-    {
-        id: 2,
-        nome: 'Guilherme',
-        idade: 21
-    }
-]
-
-
 // ENDPOINTS ----
 app.get("/", (req, res) => {
     res.send("Página inicial")
@@ -165,9 +132,4 @@ app.delete("/users/:id", (req: Request, res: Response) => {
     users.splice(userIndex);
 
     res.status
-});
-
-
-app.listen(port, () => {
-    console.log("A API subiu na porta ${port}")
 });
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import { Express, Request, Response }  from "express";      // importando tipos do Express
+import swaggerUi from "swagger-ui-express";
+
+
+const swaggerJsDoc = require("swagger-jsdoc");
+const app: Express = express();         // o aplicativo é do framework Express.js, então tem que receber o tipo do framework
+const port: number = 3000;              // a porta na qual ele vai rodar é int
+
+app.use(express.json());
+
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Clínica API",
+            version: "1.0.0",
+            description: "Aplicação fullstack de clínica.",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+        tags: [
+            { name: "Paciente", description: "Operações de administração de pacientes" },
+            { name: "Médico", description: "Operações de administração de médicos" },
+            { name: "Consulta", description: "Operações de administração de consultas" },
+        ],
+    },
+    apis: ["./src/routes/*.ts"],
+};
+
+
+const swaggerSpec = swaggerJsDoc(swaggerOptions);     // json com as configurações do swagger estabelecidas em swaggerOptions
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))     // passando a url e o json de configurações para o swagger usar
+
+app.use("/", router);
+
+app.listen(port, () => {
+    console.log("-- A API subiu na porta ${port}")
+    console.log(`-- Documentação do Swagger disponível em http://localhost:${port}/api-docs`);
+});
